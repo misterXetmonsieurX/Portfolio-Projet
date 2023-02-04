03a92f1809e9850fa06dc06decc116a55764b9b6
Moussa ALASSANE <41875933+misterXetmonsieurX@users.noreply.github.com>
2023-02-04 18:45:35 +0100

    Mise en place du clignotement alternatif

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -1,6 +1,6 @@
 const mobileMenu = document.querySelector('.burger_menu_icon i');
 const leftMobileMenu = document.querySelector('.left_mobile_menu');
-// console.log(leftMobileMenu);
+
 mobileMenu.addEventListener("click", () =>{
    if ( leftMobileMenu.style.transform === "translateX(0%)") {
     leftMobileMenu.style.transform = "translateX(-100vw)";
